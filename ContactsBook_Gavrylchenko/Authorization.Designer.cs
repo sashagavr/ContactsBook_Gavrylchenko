56b377e63d075e49cc86fea0c4dcfc2722440da9
Gavrylchenko <MyEmail@qwerty.com>
2017-09-21 10:02:07 +0300

    ContactsBook version 1.0

---
+++ ContactsBook_Gavrylchenko/Authorization.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ContactsBook_Gavrylchenko
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lLogin = new System.Windows.Forms.Label();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 145);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lLogin
+            // 
+            this.lLogin.AutoSize = true;
+            this.lLogin.Location = new System.Drawing.Point(161, 12);
+            this.lLogin.Name = "lLogin";
+            this.lLogin.Size = new System.Drawing.Size(41, 13);
+            this.lLogin.TabIndex = 1;
+            this.lLogin.Text = "Логин:";
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(161, 61);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(48, 13);
+            this.lPassword.TabIndex = 2;
+            this.lPassword.Text = "Пароль:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(164, 28);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(259, 20);
+            this.txtLogin.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(164, 77);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '♪';
+            this.txtPassword.Size = new System.Drawing.Size(259, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(164, 123);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(96, 34);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(327, 123);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(96, 34);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 169);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lPassword);
+            this.Controls.Add(this.lLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Authorization";
+            this.Text = "Authorization";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lLogin;
+        private System.Windows.Forms.Label lPassword;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ ContactsBook_Gavrylchenko/FormMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ContactsBook_Gavrylchenko
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lCategory = new System.Windows.Forms.Label();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.lContacts = new System.Windows.Forms.Label();
+            this.lbContacts = new System.Windows.Forms.ListBox();
+            this.lInformation = new System.Windows.Forms.Label();
+            this.txtInformation = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lCategory
+            // 
+            this.lCategory.AutoSize = true;
+            this.lCategory.Location = new System.Drawing.Point(29, 23);
+            this.lCategory.Name = "lCategory";
+            this.lCategory.Size = new System.Drawing.Size(69, 13);
+            this.lCategory.TabIndex = 0;
+            this.lCategory.Text = "Катергории:";
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(32, 49);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(244, 21);
+            this.cbCategory.TabIndex = 1;
+            // 
+            // lContacts
+            // 
+            this.lContacts.AutoSize = true;
+            this.lContacts.Location = new System.Drawing.Point(29, 93);
+            this.lContacts.Name = "lContacts";
+            this.lContacts.Size = new System.Drawing.Size(59, 13);
+            this.lContacts.TabIndex = 2;
+            this.lContacts.Text = "Контакты:";
+            // 
+            // lbContacts
+            // 
+            this.lbContacts.FormattingEnabled = true;
+            this.lbContacts.Location = new System.Drawing.Point(32, 122);
+            this.lbContacts.Name = "lbContacts";
+            this.lbContacts.Size = new System.Drawing.Size(244, 264);
+            this.lbContacts.TabIndex = 3;
+            // 
+            // lInformation
+            // 
+            this.lInformation.AutoSize = true;
+            this.lInformation.Location = new System.Drawing.Point(322, 23);
+            this.lInformation.Name = "lInformation";
+            this.lInformation.Size = new System.Drawing.Size(76, 13);
+            this.lInformation.TabIndex = 4;
+            this.lInformation.Text = "Информация:";
+            // 
+            // txtInformation
+            // 
+            this.txtInformation.Location = new System.Drawing.Point(325, 50);
+            this.txtInformation.Multiline = true;
+            this.txtInformation.Name = "txtInformation";
+            this.txtInformation.Size = new System.Drawing.Size(278, 336);
+            this.txtInformation.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(49, 400);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(139, 32);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(248, 400);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(139, 32);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(441, 400);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(139, 32);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 444);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInformation);
+            this.Controls.Add(this.lInformation);
+            this.Controls.Add(this.lbContacts);
+            this.Controls.Add(this.lContacts);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.lCategory);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lCategory;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.Label lContacts;
+        private System.Windows.Forms.ListBox lbContacts;
+        private System.Windows.Forms.Label lInformation;
+        private System.Windows.Forms.TextBox txtInformation;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
